3b5aae0b144177efd63e71d751f08907626be1a7
Goodguyy <106490036+Goodguyy@users.noreply.github.com>
2026-01-01 12:33:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from scraper import fetch_rates_sync
 from pathlib import Path
+from fastapi import Request
 
 app = FastAPI()
 
 # Serve static CSS
 app.mount("/static", StaticFiles(directory=Path(__file__).parent / "static"), name="static")
 
-@app.get("/")
-def home():
-    rates = fetch_rates_sync()
-    with open("templates/index.html", "r", encoding="utf-8") as f:
-        html = f.read()
+templates = Jinja2Templates(directory="templates")
 
-    # Replace placeholders in HTML
-    html = html.replace("{{FOREX_RATES}}", str(rates["data"]["forex"]))
-    html = html.replace("{{CRYPTO_USD}}", str(rates["data"]["crypto_usd"]))
-    html = html.replace("{{CRYPTO_NGN}}", str(rates["data"]["crypto_ngn"]))
-    return HTMLResponse(content=html)
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    rates = fetch_rates_sync()
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "FOREX_RATES": rates["data"]["forex"],
+        "CRYPTO_USD": rates["data"]["crypto_usd"],
+        "CRYPTO_NGN": rates["data"]["crypto_ngn"]
+    })
 
 @app.get("/rates")
 def rates():
