338795a516dfdb80312e9716bb1fbaa9b0b96d49
Goodguyy <106490036+Goodguyy@users.noreply.github.com>
2026-01-01 12:45:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from scraper import fetch_rates_sync
-from pathlib import Path
-from fastapi import Request
+
+from scraper import get_latest_rates
 
 app = FastAPI()
 
-# Serve static CSS
-app.mount("/static", StaticFiles(directory=Path(__file__).parent / "static"), name="static")
+# Serve static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Templates directory
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/", response_class=HTMLResponse)
-def home(request: Request):
-    rates = fetch_rates_sync()
-    return templates.TemplateResponse("index.html", {
-        "request": request,
-        "FOREX_RATES": rates["data"]["forex"],
-        "CRYPTO_USD": rates["data"]["crypto_usd"],
-        "CRYPTO_NGN": rates["data"]["crypto_ngn"]
-    })
+async def home(request: Request):
+    rates = await get_latest_rates()
+    return templates.TemplateResponse("index.html", {"request": request, "rates": rates})
+
 
 @app.get("/rates")
-def rates():
-    return fetch_rates_sync()
+async def rates_api():
+    return await get_latest_rates()
