83ea278c6d42ff7779d586f5e40df8b09cd37545
Goodguyy <106490036+Goodguyy@users.noreply.github.com>
2026-01-01 21:00:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,14 @@
-import os
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from scraper import fetch_all
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-app = FastAPI(title="NaijaRate")
-
-# Mount static files
-app.mount(
-    "/static",
-    StaticFiles(directory=os.path.join(BASE_DIR, "static")),
-    name="static"
-)
-
-# Templates folder
-templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-def home(request: Request):
+async def home(request: Request):
     data = fetch_all()
-    return templates.TemplateResponse(
-        "index.html",
-        {
-            "request": request,
-            "forex": data.get("forex", {}),
-            "crypto": data.get("crypto", {}),
-            "updated": data.get("updated", "N/A")
-        }
-    )
-
-@app.get("/rates", response_class=JSONResponse)
-def api_rates():
-    data = fetch_all()
-    return {
-        "forex": data.get("forex", {}),
-        "crypto": data.get("crypto", {})
-    }
+    return templates.TemplateResponse("index.html", {"request": request, "data": data})
