19b19d06880e78b8e74b609edb4f6c327b72db59
Goodguyy <106490036+Goodguyy@users.noreply.github.com>
2026-01-01 03:57:53 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,4 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent
-DATA_DIR = BASE_DIR / "data"
-DATA_DIR.mkdir(exist_ok=True)
-
-DB_PATH = DATA_DIR / "rates.db"
+# Path to your SQLite database
+DB_PATH = Path(__file__).parent / "naijarate.db"
