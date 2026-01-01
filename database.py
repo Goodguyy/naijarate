a80de4838ec98cab4694d5f5daf227aa0bd70635
Goodguyy <106490036+Goodguyy@users.noreply.github.com>
2026-01-01 11:16:25 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,31 +1,4 @@
-from sqlite_utils import Database
 from pathlib import Path
-import time
 
-DB_PATH = Path("naijarate.db")
-
-db = Database(DB_PATH)
-
-def init_db():
-    if "rates" not in db.table_names():
-        db["rates"].create(
-            {
-                "source": str,
-                "pair": str,
-                "rate": float,
-                "timestamp": int,
-            },
-            pk="id",
-            if_not_exists=True,
-        )
-
-def insert_rate(source: str, pair: str, rate: float):
-    init_db()
-    db["rates"].insert(
-        {
-            "source": source,
-            "pair": pair,
-            "rate": rate,
-            "timestamp": int(time.time()),
-        }
-    )
+BASE_DIR = Path(__file__).resolve().parent
+DB_PATH = BASE_DIR / "naijarate.db"
